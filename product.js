c7c88e31dae4c07419b2b9ba3d83392a197e3c3d
Ivenkili <emmanuelaemmanuel246@gmail.com>
2025-04-15 11:36:23 +0100

    first commit

---
+++ product.js
@@ -0,0 +1,8 @@
+product ={
+    lipgloss: "maryKay",
+    origin:"nigeria",
+    producer: "Emmanuela"
+}
+console.log(product)
+console.log(Object.values(product))
+console.log(Object.keys(product))
